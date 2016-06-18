dcbfc2ba50e27a29d653f49a3bd669ef623fd69a
choyeonjae <chojudy2@naver.com>
2016-06-19 05:10:09 +0900

    Add files via upload

---
+++ player_name.java
@@ -0,0 +1,30 @@
+class player_name{
+  String name;
+  float Money;
+  float Zen;
+
+
+}
+
+class Compant{
+  int company;
+
+}
+
+class Collect_Zen{
+  float Money;
+  float Zen;
+  int count;
+  System.out.println("Money=", Money, "Zen= ", Zen);
+  System.out.println("");
+  System.out.println("You have six ways to get Zen and Money! Here is ways");
+  System.out.println("Money=", Money, "Zen= ", Zen, "count=", count);
+  System.out.println("1.Help people (Zen +5, Money -10)");
+  System.out.println("2.Donation (Zen +Your Donation Money x 10%, Money=You want)");
+  System.out.println("3.voluntary service (Zen +10, Money -20)");
+  System.out.println("4.deep bow (Zen +1)");
+  System.out.println("5.Go back to company, and Work! (Zen -3, Money +20)");
+  System.out.println("6.Buy temple(Small temple (+Zen +10 everytime, +Money +3 everytime, Money -100)\n Large temple (+Zen +15 everytime, +Money +5 everytime, Money -300))");
+  System.out.println("");
+  System.out.println("");
+}
